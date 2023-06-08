a1ab7835ff8b2b5baf8674415a7ce72894d72ebf
FranciscoTei <102524993+FranciscoTei@users.noreply.github.com>
2023-06-08 06:18:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import telebot
-
+print('iniciando')
 TOKEN = '6021346806:AAFMBfDMzplaQ7PVKr3SsqacxgGhhscJRKQ'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 @bot.message_handler(func=lambda m:True)
 def on_message(message):
+    print('mensagem recebida')
     bot.reply_to(message, message.text)
 
 def hello_http(event, context):
+    print("evento")
     update = telebot.types.Update.de_json(event['body'])
     try:
         bot.process_new_updates([update])
